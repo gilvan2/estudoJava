c55418331cb42496b8c6447465d1438f6f473d61
gilvan.silva <gilvan.silva@e-unicred.com.br>
2022-08-08 15:19:42 -0300

    Informações importantes sobre as versões 4 e 5 do Junit

--- FormacaoJavaOrientacaoObjetos/12-java-testes-unitarios-tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ FormacaoJavaOrientacaoObjetos/12-java-testes-unitarios-tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -34,4 +34,31 @@ class BonusServiceTest {
 		
 		assertEquals(new BigDecimal("1000.00"), bonus);
 	}
+	/*
+	 No curso utilizaremos a versão 5 do JUnit, sendo que nesta nova versão da biblioteca algumas mudanças 
+	 em seus pacotes de classes foram realizadas.No JUnit 4 as classes e anotações devem ser importadas de 
+	 seu pacote org.junit. Por exemplo:
+	 
+	import org.junit.Test;
+	import org.junit.Before;
+	import org.junit.BeforeClass;
+	import org.junit.After;
+	import org.junit.AfterClass;
+	
+	Mas na versão 5 do JUnit houve uma reestruturação nos pacotes, sendo que agora os imports devem vir do pacote org.junit.jupiter.api. Exemplo:
+	
+	import org.junit.jupiter.api.Test;
+	import org.junit.jupiter.api.BeforeEach;
+	import org.junit.jupiter.api.BeforeAll;
+	import org.junit.jupiter.api.AfterEach;
+	import org.junit.jupiter.api.AfterAll;
+	
+	A única exceção foi a classe Assert, que permanece no pacote org.junit. 
+	Entretanto, no JUnit 5 uma nova classe chamada Assertions, presente no pacote org.junit.jupiter.api, 
+	foi criada e recomenda-se a sua utilização em substituição à antiga classe Assert.
+
+	Redobre a atenção em seus projetos quando for realizar o import das classes e anotações do JUnit 5, 
+	para que elas sejam importadas do pacote correto, pois, por questões de compatibilidade, 
+	as classes antigas do JUnit 4 não foram removidas da bibliotecas e ainda podem ser utilizadas.
+	*/
 }
