d2280f632be9179a717b97f201288021828fdef1
unknown <gilvan.silva.junior@gmail.com>
2022-10-23 16:55:15 -0300

    Criação de filtro para contar o tempo de cada requisição a partir de /entrada

---
+++ FormacaoSpringFrameWork/01-JavaServelet/src/main/java/br/com/JavaServelet/gerenciador/servlet/MonitoramentoFilter.java
@@ -0,0 +1,30 @@
+package br.com.JavaServelet.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = "/entrada") //Mapeado para chegar entes de entrada
+public class MonitoramentoFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		long inicio = System.currentTimeMillis();
+		String acaoAtual = request.getParameter("acao");
+		
+		chain.doFilter(request, response); //Continua com a requisição
+		
+		long fim = System.currentTimeMillis();
+		
+		System.out.println("Tempo de execução da acao: " + acaoAtual + " -> " + ( fim - inicio ));
+		
+	}
+
+}
