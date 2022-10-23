6b00afe99373106060eee60e3e713e9ed68d2911
unknown <gilvan.silva.junior@gmail.com>
2022-10-23 17:14:48 -0300

    Criação de um filter para validar se o usuáiro esta logado e retirada dessa atribuição da classe de controle

---
+++ FormacaoSpringFrameWork/01-JavaServelet/src/main/java/br/com/JavaServelet/gerenciador/servlet/AutorizacaoFilter.java
@@ -0,0 +1,45 @@
+package br.com.JavaServelet.gerenciador.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AutorizacaoFilter
+ */
+@WebFilter("/entrada")
+public class AutorizacaoFilter extends HttpFilter implements Filter {
+       
+
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+
+		//HttpServletRequest é um objeto mais especifico de uma interface ServletRequest ou seja HttpServletRequest é um ServletRequest e posso fazer casr entre eles   
+		
+		HttpServletRequest request = ( HttpServletRequest ) servletRequest; //Cast de um objet generico para mais um mais complexo do mesmo tipo
+		HttpServletResponse response = ( HttpServletResponse ) servletResponse;
+		
+		String paramAcao = request.getParameter("acao");
+		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
+		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+		
+		if(ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+
+}
