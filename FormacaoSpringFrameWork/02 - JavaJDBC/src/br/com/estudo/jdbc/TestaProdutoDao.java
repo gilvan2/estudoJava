16e89a62a7ec4a9497a83f7e0123d7f08bf652a7
Gilvan Rodrigues <gilvanrodrigues@ioasys.com.br>
2023-03-24 17:05:15 -0300

    Inserçao da camada de persistencia

--- FormacaoSpringFrameWork/02 - JavaJDBC/src/br/com/estudo/jdbc/TestaProdutoDao.java
+++ FormacaoSpringFrameWork/02 - JavaJDBC/src/br/com/estudo/jdbc/TestaProdutoDao.java
@@ -1,16 +1,15 @@
 package br.com.estudo.jdbc;
+
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.estudo.jdbc.DAO.ProdutoDAO;
 import br.com.estudo.jdbc.modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaProdutoDao {
 
 	public static void main(String[] args) throws SQLException {
-		
 		Produto comoda = new Produto("Cômoda","Cômoda Vertical");
 		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -20,9 +19,12 @@ public class TestaInsercaoComProduto {
 			ProdutoDAO produtoDAO = new ProdutoDAO(con);
 			//new PersistenciaProduto(con).salvarProduto(comoda);
 			produtoDAO.salvar(comoda);
-			//persistenciaProduto.listar();
-			
-			System.out.println(comoda);
+			//Chamoou a uma lista de produtos 
+			List<Produto> listaDeProdutos =  produtoDAO.listar();
+			//Lambia para listar de acordo com o toString que foi sobreescrito em |ProdutoDAO
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
+
 	}
+
 }
