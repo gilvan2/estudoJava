5b7bb5727db3fe94a43160e5bff093e92419b619
Gilvan Rodrigues <gilvanrodrigues@ioasys.com.br>
2023-03-26 00:48:23 -0300

    Ajuste para traser a listagem das categorias

--- FormacaoSpringFrameWork/02-loja-virtual-view-repository/src/br/com/alura/jdbc/factory/ConnectionFactory.java
+++ FormacaoSpringFrameWork/02-loja-virtual-view-repository/src/br/com/alura/jdbc/factory/ConnectionFactory.java
@@ -20,7 +20,13 @@ public class ConnectionFactory {
 		this.dataSource = comboPooledDataSource;
 	}
 
-	public Connection recuperarConexao() throws SQLException {
-		return this.dataSource.getConnection();
+	public Connection recuperarConexao() {
+	//Tratamenot da SQLException	
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
 	}
 }
