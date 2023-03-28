ca209aa80312988063cf88dfb1c5431516138679
Gilvan Rodrigues <gilvanrodrigues@ioasys.com.br>
2023-03-27 23:50:54 -0300

    criação de uma classe que realiza a persistencia no banco de dados'

--- FormacaoSpringFrameWork/03 -Persistencia com JPA - Hibernate/src/main/java/br/com/estudo/modelo/Produto.java
+++ FormacaoSpringFrameWork/03 -Persistencia com JPA - Hibernate/src/main/java/br/com/estudo/modelo/Produto.java
@@ -18,6 +18,30 @@ public class Produto {
 	private String nome;
 	private String descricao;
 	private BigDecimal preco;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 	
 	//Caso o nome do campo seja diferente do que esta na aentidade, usar a anootação column com o nome do campo no banco de dados
 	//Para projetos em hibernate, não precisamos indicar o path da classe no persistence.xml
--- FormacaoSpringFrameWork/03 -Persistencia com JPA - Hibernate/src/main/java/br/com/estudo/testes/CadastroDeprodutos.java
+++ FormacaoSpringFrameWork/03 -Persistencia com JPA - Hibernate/src/main/java/br/com/estudo/testes/CadastroDeprodutos.java
@@ -2,6 +2,10 @@ package br.com.estudo.testes;
 
 import java.math.BigDecimal;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import br.com.estudo.modelo.Produto;
 
 public class CadastroDeprodutos {
@@ -9,6 +13,24 @@ public class CadastroDeprodutos {
 	public static void main(String[] args) {
 
 		Produto celular = new Produto();
+		celular.setNome("Xiaomi Redmi");
+		celular.setDescricao("Muito legak");
+		celular.setPreco(new BigDecimal("800"));
+		
+		
+		/**
+		 * Para se criar uma conexão com o banco de dados, precisamos implementar uma interface e usar o método descrito embaixo para buscar o nome da
+		 * entidade cadastrada no persistence-unit presente no arquivo persistence.xml*/
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+		
+		EntityManager em = factory.createEntityManager();
+		
+		//Pelo fato do nosso controle de transação sem RESOURCE_LOCAL, obrigatóriamente indicamos quando a trnsação começa e quando termina
+		
+		em.getTransaction().begin();
+		em.persist(celular);
+		em.getTransaction().commit();
+		em.close();
 
 	}
 
