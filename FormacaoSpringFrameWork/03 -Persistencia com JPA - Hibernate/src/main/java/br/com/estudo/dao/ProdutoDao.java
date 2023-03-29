69add644e40f18eb25d633c0e1442b35c74dd202
Gilvan Rodrigues <gilvanrodrigues@ioasys.com.br>
2023-03-29 01:34:54 -0300

    Ajustes para ctriar metodo que raliza pesquisa do atributo da classe se passando somento os campos que serão devolvidos

--- FormacaoSpringFrameWork/03 -Persistencia com JPA - Hibernate/src/main/java/br/com/estudo/dao/ProdutoDao.java
+++ FormacaoSpringFrameWork/03 -Persistencia com JPA - Hibernate/src/main/java/br/com/estudo/dao/ProdutoDao.java
@@ -1,5 +1,6 @@
 package br.com.estudo.dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -45,4 +46,12 @@ public class ProdutoDao {
 		
 		return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
 	}
+	
+	public BigDecimal buscarPrecoDoProdutoComNome(String nome){
+		//JPQL -> Monta uma query parecido com sql
+		
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+		
+		return em.createQuery(jpql, BigDecimal.class).setParameter("nome", nome).getSingleResult();
+	}	
 }
