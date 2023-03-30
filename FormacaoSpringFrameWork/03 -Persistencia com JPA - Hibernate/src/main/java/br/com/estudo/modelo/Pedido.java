a5bc7a34cee24208c894c7ac9ec4763a115f32c4
Gilvan Rodrigues <gilvanrodrigues@ioasys.com.br>
2023-03-29 23:55:36 -0300

    criação da entidade cliente e pedido

---
+++ FormacaoSpringFrameWork/03 -Persistencia com JPA - Hibernate/src/main/java/br/com/estudo/modelo/Pedido.java
@@ -0,0 +1,66 @@
+package br.com.estudo.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal valorTotal;
+	private LocalDate data = LocalDate.now();
+
+	@ManyToOne
+	private Cliente cliente;
+
+	public Pedido(Cliente cliente) {
+		super();
+		this.cliente = cliente;
+	}
+
+	public Pedido() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValor_total() {
+		return valor_total;
+	}
+
+	public void setValor_total(BigDecimal valor_total) {
+		this.valor_total = valor_total;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
