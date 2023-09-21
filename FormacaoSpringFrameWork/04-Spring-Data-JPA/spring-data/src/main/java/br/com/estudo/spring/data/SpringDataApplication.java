697b83badf6744496d51ee82021382b811f4fa3d
gilvan.rodrigues <gilvan.silva.junior@gmail.com>
2023-09-21 15:41:28 -0300

    Exemplo de população de dados para a entidade Cargos

--- FormacaoSpringFrameWork/04-Spring-Data-JPA/spring-data/src/main/java/br/com/estudo/spring/data/SpringDataApplication.java
+++ FormacaoSpringFrameWork/04-Spring-Data-JPA/spring-data/src/main/java/br/com/estudo/spring/data/SpringDataApplication.java
@@ -1,13 +1,29 @@
 package br.com.estudo.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.estudo.spring.data.orm.Cargo;
+import br.com.estudo.spring.data.repository.CargoRepository;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner{
+	
+	private final CargoRepository repository;
+	
+	public SpringDataApplication(CargoRepository repository) {
+		this.repository = repository; //Injeção de dependência para uma instacia de classe que impelemente uma interface CargoRepository
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		repository.save(cargo);//Com a injeção feita, posso usar as implementações da interface CargoRepository
+	}
 }
